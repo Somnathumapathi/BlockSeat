e0517f8351806e9a83ad55d2c22c69c9d4ab7e0e
Somnathumapathi <somnathumapathi7@gmail.com>
2024-09-06 16:15:37 +0530

    wallet setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ethers} from 'ethers'
+import config from './config.json'
+import { useEffect, useState } from 'react';
+import TokenTix from './utils/tokentixABI.json'
+
 
 function App() {
+  const [account, setAccount] = useState("")
+  const loadBlockchain = async () => {
+    const accounts  = await window.ethereum.request({method: 'eth_requestAccounts'})
+    const accAddress = ethers.getAddress(accounts[0])
+    console.log(accAddress)
+    setAccount(accAddress)
+
+    window.ethereum.on('accountsChanged', async () => {
+      console.log('hi')
+      const accounts  = await window.ethereum.request({method: 'eth_requestAccounts'})
+    const accAddress = ethers.getAddress(accounts[0])
+    console.log(accAddress)
+    setAccount(accAddress)
+    })
+  }
+  useEffect(() => {
+loadBlockchain()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <p>{account}</p>
     </div>
   );
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,79 @@
-import { ethers } from "hardhat";
+import { ethers } from "hardhat"
 
 const hre = require("hardhat")
 
+const tokens = (n : any) => {
+  return ethers.parseUnits(n.toString(), 'ether')
+}
 
 async function main() {
-    const TokenTix = await hre.ethers.getContractFactory("TokenTix")
-    const tokenTix = await TokenTix.deploy()
-    
-    await tokenTix.deployed()
-  
-    console.log(tokenTix.address)
+
+  const [deployer] = await ethers.getSigners()
+
+  const TokenTix = await ethers.getContractFactory("TokenTix")
+  const tokenTix = await TokenTix.deploy("TokenTix", "TT")
+
+  await tokenTix.waitForDeployment()
+
+  console.log(`Deployed TokenTix Contract at: ${tokenTix.target}\n`)
+
+  const occasions = [
+    {
+      name: "IPL finals",
+      price: tokens(3),
+      noOfTickets: 0,
+      date: "May 31",
+      time: "6:00PM IST",
+      location: "Chinnaswamy Stadium"
+    },
+    {
+      name: "G.O.A.T",
+      price: tokens(1),
+      noOfTickets: 125,
+      date: "Jun 2",
+      time: "1:00PM IST",
+      location: "Swagath Poornima, Bangalore"
+    },
+    {
+      name: "ETH India",
+      price: tokens(0.25),
+      noOfTickets: 200,
+      date: "Jun 9",
+      time: "10:00AM IST",
+      location: "KTPO, Bangalore"
+    },
+    {
+      name: "Mangalore-Ooty",
+      price: tokens(5),
+      noOfTickets: 0,
+      date: "Jun 11",
+      time: "2:30PM IST",
+      location: "On road, South India"
+    },
+    {
+      name: "ETH Global",
+      price: tokens(1.5),
+      noOfTickets: 125,
+      date: "Jun 23",
+      time: "11:00AM IST",
+      location: "Somewhere, Chennai"
+    }
+  ]
+
+  for (var i = 0; i < 5; i++) {
+    const transaction = await tokenTix.connect(deployer).createOccasion(
+      occasions[i].name,
+      occasions[i].price,
+      occasions[i].noOfTickets,
+      occasions[i].date,
+      occasions[i].time,
+      occasions[i].location,
+    )
+
+    await transaction.wait()
+
+    console.log(`Event ${i + 1}: ${occasions[i].name}`)
+  }
 }
 
 main().catch((error) => {
